ebe401c7eeae4542fe300338d229fde608f9ddeb
John Moon <john.moon@livingsocial.com>
2014-05-30 17:40:24 -0400

    Initial Commit

---
+++ Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by John Moon on 5/30/14.
+//  Copyright (c) 2014 BNR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}

a9826cc651245153f3e0ea9aa962fa7d38d83aa4
John Moon <john.moon@livingsocial.com>
2014-06-14 09:10:03 -0400

    Initial Commit

---
+++ TouchTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchTracker
+//
+//  Created by John Moon on 6/14/14.
+//  Copyright (c) 2014 JM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
